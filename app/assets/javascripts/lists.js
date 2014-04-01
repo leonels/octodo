a7c113dc8a875e4abb3894773f4ac274b6f6f52e
Leonel Santos <leonelsantos.me@gmail.com>
2014-04-01 14:12:42 -0500

    AngularJS based filter and sort

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,11 +1,18 @@
 var octodoApp = angular.module('octodoApp', ['ngResource']);
 
-octodoApp.controller('ListsCtrl', ['$scope', 'List', function($scope, List) {
+// octodoApp.controller('ListsCtrl', ['$scope', 'List', function($scope, List) {
+octodoApp.controller('ListsCtrl', function($scope, $http) {
   $scope.heading = 'Lists';
-  $scope.lists = List.query();
-}]);
+  // $scope.lists = List.query();
 
-octodoApp.factory('List', function($resource){
+  $http.get('/lists.json').success(function(data) {
+  	$scope.lists = data;
+  });
+
+  $scope.orderProp = 'title';
+});
+
+// octodoApp.factory('List', function($resource){
   /*
 
     if the controller is only responding with json, $resource('/lists') would have been enough
@@ -15,5 +22,5 @@ octodoApp.factory('List', function($resource){
     save yourself another 45 minutes of troubleshootin! 
   
   */
-  return $resource('/lists.json');
-});
+//  return $resource('/lists.json');
+// });
